8b7fce34c2adfa6a31fc67a52ecb89d4fce43c3f
4nonymoustechie <ibrahimbusari4@gmail.com>
2023-11-17 00:50:19 -0800

    i got a new api that has dollar value and naira value and its working perfectly

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
-const forInp = document.querySelector('.inputer')
+let forInp = document.querySelector('.inputer')
 //let  output = document.querySelector('h1')
 const usd = document.querySelector('.usdbtn')
 const ngn = document.querySelector('.ngnbtn')
 const forbtt = document.querySelector('.btn')
-// const forConvert = async() => {
-// const forApi =   await fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
 
-// const convertAndRead  =  await  forApi.json()
+const forConvert = async() => {
+const forApi =   await fetch("https://api.currencyapi.com/v3/latest?apikey=cur_live_uaXdDC3aZvNuD4TPBIaSn8Y0oyzV7PryRxsfYuru")
 
-// console.dir(convertAndRead)
-// return(convertAndRead.bpi.USD.rate_float)
+const convertAndRead  =  await  forApi.json()
 
-// }
+console.dir(convertAndRead)
+//console.log(convertAndRead.data.ZAR.value)
 
-// forConvert()
+return convertAndRead.data.NGN.value
+
+}
+
+const forConverting = async() => {
+    const forApis =   await fetch("https://api.currencyapi.com/v3/latest?apikey=cur_live_uaXdDC3aZvNuD4TPBIaSn8Y0oyzV7PryRxsfYuru")
+    
+    const convertAndReadIt  =  await  forApis.json()
+    
+    console.dir(convertAndReadIt)
+    //console.log(convertAndRead.data.ZAR.value)
+    
+    return(convertAndReadIt.data.NGN.value)
+    
+    }
 
 
-//let dollarVal = forInp.value * 845.44 
 
-usd.addEventListener('click', function(){
-forbtt.addEventListener('click', () =>{
 
-document.querySelector('h1').textContent =   forInp.value * 845.44  
+ngn.addEventListener('click', async() =>{
+forbtt.addEventListener('click', async () => {
+
+    let forNgn = await forConvert()
+    
+
+document.querySelector('h1').textContent =   forInp.value / forNgn
 
 } )
  
-
 })
+usd.addEventListener('click', async() => {
+    forbtt.addEventListener('click', async() => {
+
+        let forUsd = await forConverting()
+
+    document.querySelector('h1').textContent =   forInp.value  * forUsd 
+
+
+    } )
+     
+    
+    })
